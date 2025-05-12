479ae634f03682c6b611e1a0854bb1676bb1d790
michaelcdevin <mcdevin@sandia.gov>
2025-05-11 21:26:53 -0600

    updated S-Function build script

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -18,7 +18,7 @@
 
 mexname = 'FAST_SFunc'; % base name of the resulting mex file
 
-built_with_visualStudio = false; %if the libraries were built with cmake, set to false
+built_with_visualStudio = true; %if the libraries were built with cmake, set to false
 
 
 if (ispc && built_with_visualStudio)   
